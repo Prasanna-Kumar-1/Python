2471411fbd139c1804700d9338d460348f16efab
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-04-19 09:43:56 +1000

    Implementation of Boolean Concept

---
+++ Boolean.py
@@ -0,0 +1,9 @@
+# Implementation of Boolean Expressions
+day = "Sunday"
+temperature = 17
+raining = True
+
+if (day == "Sunday" and temperature > 30) and not raining:
+    print("Time for Fun. Go out and have couple of beers")
+else:
+    print("Stay home and Learn Python. Its awesome")
