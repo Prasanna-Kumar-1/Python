91f50b3746be2da4e5998795fb1ead70ab778c30
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-05-03 20:11:21 +1000

    Implementation of *args

---
+++ ArgsImplementation.py
@@ -0,0 +1,23 @@
+# *args allows us to use arbitrary number of arguments, instead of using fixed number of positional parameters
+def myfunc(*args):
+    return sum(args) * 0.10
+
+
+print(myfunc(60, 40))
+print(myfunc(60, 40, 50, 50))
+
+# args are passed as Tuples
+# Result of the below code is : (10, 20, 30)
+def myfunc1(*args):
+    print(args)
+
+
+myfunc1(10, 20, 30)
+
+
+def myfunc2(*args):
+    for item in args:
+        print(item)
+
+
+myfunc2(10, 20, 30)
