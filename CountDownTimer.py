879a28166ecd769177f50a7d4e0078efa7f5a504
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-06-11 20:48:14 +1000

    Implementation of Count Down Timer

---
+++ CountDownTimer.py
@@ -0,0 +1,8 @@
+# Implementation of Count Down Timer
+from time import sleep
+
+for i in range(10, 0, -1):
+    print(f"{i} Seconds to Lift Off!")
+    sleep(1)
+
+print('Sydney, We have a Lift off!')
