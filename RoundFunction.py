e372a907d041980fb4c158491fb291e6d7692050
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-06-22 22:58:22 +1000

    Implementation of round() function

---
+++ RoundFunction.py
@@ -0,0 +1,18 @@
+# Implementation of round() function
+
+# rounding to next 10
+number1 = round(28, -1)
+print("rounding of {} to next 10 is : ".format(number1))
+
+# rounding to next 10
+number2 = round(288, -1)
+print("rounding of {} to next 10 is : ".format(number2))
+
+# rounding to next 10
+number3 = round(288, -2)
+print("rounding of {} to next 10 is : ".format(number3))
+
+# rounding to next 1000
+number4 = round(2888, -3)
+print("rounding of {} to next 10 is : ".format(number4))
+
