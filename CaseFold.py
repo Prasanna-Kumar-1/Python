413b421194ae3a65a0c6197bd6f7c6e17bbe1cc0
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-07-06 19:58:15 +1000

    Implementation of casefold() function

---
+++ CaseFold.py
@@ -0,0 +1,7 @@
+# Implementation of casefold() function
+
+first_string = 'PrasaNNa'
+second_string = 'praSAnnA'
+
+if first_string.casefold() == second_string.casefold():
+    print("Two Strings are equal")
