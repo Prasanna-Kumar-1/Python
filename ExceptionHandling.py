e738fd0940b6c96c3c740f8af550876d6593d202
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-11-08 22:22:25 +1100

    Implementation of 'Exception Handling' function concepts

---
+++ ExceptionHandling.py
@@ -0,0 +1,24 @@
+# import module sys to get the type of exception
+import sys
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1 / int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
+print("----------------------------------------------------------")
+
+# Raising Specific Exceptions in Python
+try:
+    a = int(input("Enter a positive integer: "))
+    if a <= 0:
+        raise ValueError("That is not a positive number!")
+except ValueError as ve:
+    print(ve)
