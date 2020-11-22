167f156b6d2e89d8787d8cf555c37deb66970f6c
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-11-22 13:28:10 +1100

    Implementation of 'Exception Handling' function concepts

--- ExceptionHandling2.py
+++ ExceptionHandling2.py
@@ -1,13 +1,17 @@
 def ask_for_int():
-    try:
-        result = int(input("Please Enter any number: "))
-    except:
-        print("Whoops! That is not a number")
-    else:
-        print("You have correctly entered number. Thank you")
-    finally:
-        print("End of try/except/finally block")
-        print("I always run at the end")
+    while True:
+        try:
+            result = int(input("Please Enter any number: "))
+        except:
+            print("Whoops! That is not a number")
+            continue
+        else:
+            print("You have correctly entered number. Thank you")
+            break
+        finally:
+            print("End of try/except/finally block")
+            print("I always run at the end")
 
 
-ask_for_int()
+if __name__ == "__main__":
+    ask_for_int()
