d906542a2d78cf3f497212e103751e323db3448e
prasanna1312 <prasanna.kumar@outlook.com.au>
2020-12-21 22:12:25 +1100

    modified of 'Dictionary Comprehension' Concepts

---
+++ dictionarycomprehension.py
@@ -0,0 +1,5 @@
+# Dictionary comprehension
+# Dictionary comprehensions are similar like list comprehension, with the additional requirement of defining a key
+
+squares = {i: i * i for i in range(10)}
+print(squares)
